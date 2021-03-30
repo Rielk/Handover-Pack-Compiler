77b0c6933459e3108a43a73eaf024143f02aa2db
Redflameman0 <william@symbotics.co.uk>
2021-03-30 18:45:41 +0100

    Moved Compare to a string extension method.

--- DataFormats.cs
+++ DataFormats.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using ExtensionMethods;
 
 public class Data : IEquatable<InverterData>, IComparable<InverterData>, IComparable
 {
@@ -39,48 +40,9 @@ public class Data : IEquatable<InverterData>, IComparable<InverterData>, ICompar
         if (!(obj is InverterData objAsPart)) return 1;
         else return CompareTo(objAsPart);
     }
-    public int CompareTo(InverterData ComparePart)
+    public int CompareTo(InverterData CompareData)
     {
-        string x = Name;
-        string y = ComparePart.Name;
-        for (int pos = 0; pos < x.Length && pos < y.Length; pos++)
-        {
-            //If the values are the same, then conitnue down string.
-            if (x[pos] == y[pos])
-            {
-                continue;
-            }
-            //As long as both characters aren't digits, compare normally.
-            if (!(char.IsDigit(x[pos]) && char.IsDigit(y[pos])))
-            {
-                return x[pos].CompareTo(y[pos]);
-            }
-
-            //If both characters are digits, then compare the next numbers as one.
-            //Find the full number from the foolowing digits
-            int numx = 0;
-            for (int temp_pos = pos; temp_pos < x.Length && char.IsDigit(x[temp_pos]); temp_pos++)
-            {
-                numx *= 10; numx += (int)Char.GetNumericValue(x[temp_pos]);
-            }
-            int numy = 0;
-            for (int temp_pos = pos; temp_pos < y.Length && char.IsDigit(y[temp_pos]); temp_pos++)
-            {
-                numy *= 10; numy += (int)Char.GetNumericValue(y[temp_pos]);
-            }
-            //If the numbers are the same, move the pos to end of number, then continue down string.
-            if (numx == numy)
-            {
-                pos += (int)Math.Floor((double)numx / 10);
-                continue;
-            }
-            //Otherwise, compare them
-            else
-            {
-                return numx.CompareTo(numy);
-            }
-        }
-        return x.Length - y.Length;
+        return Name.NumericCompare(CompareData.Name);
     }
 }
 
