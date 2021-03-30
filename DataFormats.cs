a93869ce2801120f5cafa94949c0fa3719e90f65
Redflameman0 <william@symbotics.co.uk>
2021-03-30 18:22:44 +0100

    Inverter List sorting

--- DataFormats.cs
+++ DataFormats.cs
@@ -6,11 +6,9 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-public class InverterData : IEquatable<InverterData>, IComparable<InverterData>, IComparable
+public class Data : IEquatable<InverterData>, IComparable<InverterData>, IComparable
 {
     public string Name { get; set; } = null;
-    public string Datasheet { get; set; } = null;
-    public bool SolarEdge { get; set; } = false;
     public override string ToString()
     {
         return Name.ToString();
@@ -43,9 +41,51 @@ public class InverterData : IEquatable<InverterData>, IComparable<InverterData>,
     }
     public int CompareTo(InverterData ComparePart)
     {
-        if (ComparePart == null)
-            return 1;
-        else
-            return this.Name.CompareTo(ComparePart.Name);
+        string x = Name;
+        string y = ComparePart.Name;
+        for (int pos = 0; pos < x.Length && pos < y.Length; pos++)
+        {
+            //If the values are the same, then conitnue down string.
+            if (x[pos] == y[pos])
+            {
+                continue;
+            }
+            //As long as both characters aren't digits, compare normally.
+            if (!(char.IsDigit(x[pos]) && char.IsDigit(y[pos])))
+            {
+                return x[pos].CompareTo(y[pos]);
+            }
+
+            //If both characters are digits, then compare the next numbers as one.
+            //Find the full number from the foolowing digits
+            int numx = 0;
+            for (int temp_pos = pos; temp_pos < x.Length && char.IsDigit(x[temp_pos]); temp_pos++)
+            {
+                numx *= 10; numx += (int)Char.GetNumericValue(x[temp_pos]);
+            }
+            int numy = 0;
+            for (int temp_pos = pos; temp_pos < y.Length && char.IsDigit(y[temp_pos]); temp_pos++)
+            {
+                numy *= 10; numy += (int)Char.GetNumericValue(y[temp_pos]);
+            }
+            //If the numbers are the same, move the pos to end of number, then continue down string.
+            if (numx == numy)
+            {
+                pos += (int)Math.Floor((double)numx / 10);
+                continue;
+            }
+            //Otherwise, compare them
+            else
+            {
+                return numx.CompareTo(numy);
+            }
+        }
+        return x.Length - y.Length;
     }
+}
+
+public class  InverterData : Data
+{
+    public string Datasheet { get; set; } = null;
+    public bool SolarEdge { get; set; } = false;
 }
