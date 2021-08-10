6a542100918204ea71e4404cd899d62219f146ce
Rielk <william@symbotics.co.uk>
2021-08-10 18:42:02 +0100

    Begun affing DropSelector for multiple files when only one allowed.

---
+++ Controls/PartSelectors/FileDropSelector.Designer.cs
@@ -0,0 +1,87 @@
+﻿
+namespace Handover_Pack_Compiler
+{
+    partial class FileDropSelector
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.OpenButton = new System.Windows.Forms.Button();
+            this.DropBox = new System.Windows.Forms.ComboBox();
+            this.Button = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // OpenButton
+            // 
+            this.OpenButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.OpenButton.Location = new System.Drawing.Point(412, 0);
+            this.OpenButton.Name = "OpenButton";
+            this.OpenButton.Size = new System.Drawing.Size(41, 20);
+            this.OpenButton.TabIndex = 8;
+            this.OpenButton.Text = "Open";
+            this.OpenButton.UseVisualStyleBackColor = true;
+            this.OpenButton.Click += new System.EventHandler(this.OpenButton_Click);
+            // 
+            // DropBox
+            // 
+            this.DropBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.DropBox.FormattingEnabled = true;
+            this.DropBox.Location = new System.Drawing.Point(3, 0);
+            this.DropBox.Name = "DropBox";
+            this.DropBox.Size = new System.Drawing.Size(371, 21);
+            this.DropBox.TabIndex = 9;
+            // 
+            // Button
+            // 
+            this.Button.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.Button.Location = new System.Drawing.Point(380, 0);
+            this.Button.Name = "Button";
+            this.Button.Size = new System.Drawing.Size(26, 20);
+            this.Button.TabIndex = 10;
+            this.Button.Text = "...";
+            this.Button.UseVisualStyleBackColor = true;
+            // 
+            // FileDropSelector
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.Button);
+            this.Controls.Add(this.DropBox);
+            this.Controls.Add(this.OpenButton);
+            this.MaximumSize = new System.Drawing.Size(5000, 21);
+            this.MinimumSize = new System.Drawing.Size(0, 21);
+            this.Name = "FileDropSelector";
+            this.Size = new System.Drawing.Size(453, 21);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button OpenButton;
+        private System.Windows.Forms.ComboBox DropBox;
+        private System.Windows.Forms.Button Button;
+    }
+}
---
+++ Controls/PartSelectors/FileDropSelector.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Handover_Pack_Compiler
+{
+    public partial class FileDropSelector : UserControl
+    {
+        public string Value
+        {
+            get { return CSPath.FullPath; }
+        }
+        public CommSitePath CSPath = new CommSitePath();
+        public int? DefaultFolder = null;
+        public string Filter = "All files (*.*)|*.*|PDF files (*.pdf)|*.pdf";
+        private readonly OpenFileDialog file_dialog = new OpenFileDialog();
+        public bool enbld = false;
+        public bool ReadOnly
+        {
+            get { return !enbld; }
+            set
+            {
+                enbld = !value;
+                TextBox.Enabled = !value;
+                Button.Enabled = !value;
+                //OpenButton.Enabled = !value;
+                RemoveButton.Enabled = !value;
+            }
+        }
+        public FileDropSelector()
+        {
+            InitializeComponent();
+        }
+        [Browsable(true)]
+        [Category("Action")]
+        [Description("Invoked when Value updates")]
+        public event EventHandler ValueUpdate;
+
+        [Browsable(true)]
+        [Category("Action")]
+        [Description("Invoked when Value updates")]
+        public event EventHandler RemoveThis;
+
+        private void Button_Click(object sender, EventArgs e)
+        {
+            file_dialog.Filter = Filter;
+            file_dialog.FilterIndex = 0;
+            file_dialog.InitialDirectory = DefaultPath.CustomerFile(Value, DefaultFolder);
+            if (file_dialog.ShowDialog() == DialogResult.OK)
+            {
+                Value = file_dialog.FileName;
+                TextBox.Text = CSPath.FileName;
+                ValueUpdate?.Invoke(this, null);
+            }
+        }
+
+        private void RemoveButton_Click(object sender, EventArgs e)
+        {
+            RemoveThis?.Invoke(this, null);
+        }
+
+        private void OpenButton_Click(object sender, EventArgs e)
+        {
+            if (File.Exists(Value))
+            {
+                Process.Start(Value);
+            }
+            else
+            {
+                MessageBox.Show("Can no longer find the file at \""+Value+"\". It may have been deleted or renamed.", "Something went wrong");
+            }
+        }
+    }
+}
